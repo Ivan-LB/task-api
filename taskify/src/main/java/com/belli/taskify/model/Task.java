8d0a4f1e8c9cfe22627b2c369ae3275c5c4ad579
Belli <ivanlorenzana@outlok.com>
2024-12-19 01:20:46 -0800

    Remove Lombok plugin and implementation of getters and setters manually

--- taskify/src/main/java/com/belli/taskify/model/Task.java
+++ taskify/src/main/java/com/belli/taskify/model/Task.java
@@ -2,17 +2,9 @@ package com.belli.taskify.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "tasks")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +17,46 @@ public class Task {
 
     @NotBlank(message = "Status cannot be blank!")
     private String status;
+
+    public Task() {
+
+    }
+
+    public Task(Long id, String title, String description, String status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
